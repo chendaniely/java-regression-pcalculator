30625d158f0d036feaffe51d2bd0cdfbc113449a
Daniel Chen <Maveri2k@yahoo.com>
2014-01-03 02:42:03 -0500

    Class to test implemtation (moved main function call here)

---
+++ src/probability/TestClass.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package probability;
+import java.io.IOException;
+
+/**
+ * Class to test ProbabilityFromRegressionOutput
+ * 
+ * @author dchen
+ * 
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		// print current working directory
+		System.out.println(System.getProperty("user.dir"));
+		Object ta = new TestAgent();
+
+		// want to take this list and also convert it to the variables used for
+		// the agent class
+		// variables used in the stats output
+		String[] variables = { "agecat", "race4", "male", "inccat4", "educat3",
+				"agecat*race4" };
+
+		ProbabilityFromRegressionOutput p = new ProbabilityFromRegressionOutput(ta, variables,
+				"./src/alternativeVariableNames.csv",
+				"./src/SASRegressionExample.csv", 4, 122, 1, 5);
+
+	}
+
+}
