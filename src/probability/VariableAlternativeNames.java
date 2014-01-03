ffd981c0d7248fe48a0bacc1d1b8a8dff195c5ff
Daniel Chen <Maveri2k@yahoo.com>
2014-01-02 19:33:17 -0500

    Class to store alternative agent variable names used for hashtable

---
+++ src/probability/VariableAlternativeNames.java
@@ -0,0 +1,22 @@
+package probability;
+
+import java.util.ArrayList;
+
+public class VariableAlternativeNames {
+	
+	private String value = "";
+	ArrayList<String> possibleValues = new ArrayList<String>();
+	
+	public VariableAlternativeNames(){
+		
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
