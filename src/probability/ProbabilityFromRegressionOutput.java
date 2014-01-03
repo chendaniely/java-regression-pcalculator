447f0121f1ade223f3fccce33febcec041fc0b34
Daniel Chen <Maveri2k@yahoo.com>
2014-01-02 23:11:16 -0500

    implemented categorical variables, working on interaction terms
    check for interaction clause before doing the categorical variables
    since "String agentVariable =
    variableSynonym.get(statsVariable).toString().trim();" will throw an
    error since the actual interaction term is not in the lookup table.  if
    I encounter an interaction term, i split by the '*' and then look up
    each term separately

--- src/probability/ProbabilityFromRegressionOutput.java
+++ src/probability/ProbabilityFromRegressionOutput.java
@@ -5,9 +5,7 @@ package probability;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.Iterator;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -26,7 +24,7 @@ public class ProbabilityFromRegressionOutput {
 		TestAgent ta = new TestAgent();
 		
 		// want to take this list and also convert it to the variables used for the agent class
-		String[] variables = {"agecat", "race4", "agecat*race4"};
+		String[] variables = {"agecat", "race4", "male", "inccat4", "educat3", "agecat*race4"};
 
 		ProbabilityFromRegressionOutput(ta, variables, "./src/alternativeVariableNames.csv","./src/SASRegressionExample.csv",
 				4, 122, 1, 5);
@@ -36,20 +34,23 @@ public class ProbabilityFromRegressionOutput {
 	/**
 	 * 
 	 * @param agent
-	 *            when used in an agent-based model, this is the agent object
+	 *            object: when used in an agent-based model, this is the agent
+	 *            object
 	 * @param variables
-	 *            variables to be read from the csv file
+	 *            array: variables to be read from the csv file
 	 * @param file
-	 *            file directory of where parameter estimate table is
+	 *            string: file directory of where parameter estimate table is
 	 * @param rowStart
-	 *            row where to start looking for parameter variables and beta
-	 *            estimates
+	 *            int: row where to start looking for parameter variables and
+	 *            beta estimates
 	 * @param rowEnd
+	 *            int: row where to stop looking for parameter variables and
+	 *            beta estimates
 	 * @param colStart
-	 *            column where variables are
+	 *            int: column where variables are
 	 * @param colEnd
-	 *            column where to stop reading (where the parameter estimates
-	 *            are)
+	 *            int: column where to stop reading (where the parameter
+	 *            estimates are)
 	 * @throws IOException
 	 * @author dchen
 	 * 
@@ -59,43 +60,92 @@ public class ProbabilityFromRegressionOutput {
 			int rowStart, int rowEnd, int colStart, int colEnd)
 			throws IOException {
 		
-//		hardCodedLookups(ta, variables, alternativeName, file, rowStart, rowEnd, colStart, colEnd);
+		hardCodedLookups(ta, variables, alternativeName, file, rowStart, rowEnd, colStart, colEnd);
 		genericCodedLookups(ta, variables, alternativeName, file, rowStart, rowEnd, colStart, colEnd);
 		
 	}
-	
-	public static void genericCodedLookups (TestAgent ta,
+	/**
+	 * 
+	 * @param ta
+	 * @param variables
+	 * @param alternativeName
+	 * @param file
+	 * @param rowStart
+	 * @param rowEnd
+	 * @param colStart
+	 * @param colEnd
+	 * @throws IOException
+	 */
+	private static void genericCodedLookups (TestAgent ta,
 			String[] variables, String alternativeName, String file,
 			int rowStart, int rowEnd, int colStart, int colEnd)
 			throws IOException {
 		
-		double logitP = 0.0;
-		String value = "";
+		/*
+		 * List of beta estimates, including b_0 (the intercept)
+		 */
+		CSVReader reader = new CSVReader(new FileReader(file));
+		Hashtable hashtable = new Hashtable();
+
+		String[] nextLine;
+		int i = 0;
+		// int start = 5;
+		// int end = 15;
+		// assumes first 3 rows are: the variable, categorical status 1, cat status 2
+		// last row is the column of beta estimates
+		// puts data into hashtable
+		while ((nextLine = reader.readNext()) != null) {
+			// -2 is used so you can just use the line number in the file
+			// corrects for the csci indexing and first col variable names
+			if ((i > (rowStart - 2)) && (i < rowEnd)) {
+				// nextLine[] is an array of values from the line
+
+//				String str = Arrays.toString(nextLine);
+//				System.out.println(str);
+//				System.out.println(nextLine[0]);
+
+				StatsOutput statsOutput = new StatsOutput();
+				statsOutput.setParameter(nextLine[0]);
+				if (nextLine[1].trim().length() > 0) {
+					statsOutput.setP1(Integer.parseInt(nextLine[1]));
+				}
+				if (nextLine[2].trim().length() > 0) {
+					statsOutput.setP2(Integer.parseInt(nextLine[2]));
+				}
+
+				statsOutput.setEstimate(Double.parseDouble(nextLine[4]));
+				
+				// key: nextLine[0] + nextLine[1] + nextLine[2]
+				// value: statsOutput
+				hashtable.put(nextLine[0] + nextLine[1] + nextLine[2], statsOutput);
+
+			}
+			i++;
+		}
+		
+		Double logitP = 0.0;
+		Double value = 0.0;
+		
 		System.out.println("@@@@@ BEGIN GENERIC LOOKUPS @@@@@");
 		
-		CSVReader variableSynonymCSV = new CSVReader(new FileReader(alternativeName));
+		CSVReader variableSynonymCSV = new CSVReader(new FileReader(alternativeName), ',', '\"', 1);
 		Hashtable variableSynonym = new Hashtable();
 		
 		String[] nextVariableLine;
 
-		int i = 0;
+		i = 0;
 		// -2 is used so you can just use the line number in the file, it corrects for the csci indexing
 		
-		System.out.println("read in variable synonym csv");
-		
-		Boolean isFirstLine = true;
-		
+//		System.out.println("read in variable synonym csv");
+//		
+//		System.out.println("skkipped first line");
 		while ((nextVariableLine = variableSynonymCSV.readNext()) != null) {
-			if (isFirstLine == true){
-				System.out.println("skipping first line");
-				isFirstLine = false;
-				continue;
-			}
+
 			
 			VariableAlternativeNames variableAlternativeNames = new VariableAlternativeNames();
 			
 			for (int j = 0; j < nextVariableLine.length; j++){
-				System.out.print(nextVariableLine[j] + " ");
+//				System.out.print(nextVariableLine[j] + " ");
 				if (j == 0){
 					variableAlternativeNames.setValue(nextVariableLine[0].trim());
 					continue;
@@ -105,19 +155,115 @@ public class ProbabilityFromRegressionOutput {
 					variableSynonym.put(nextVariableLine[j].trim(), nextVariableLine[0].trim());
 				}
 			}
-			System.out.println("");
+//			System.out.println("");
 			i++;
 		}
 		
-//		if (variableSynonym.containsKey("race4")){
-//			value = ((VariableAlternativeNames) variableSynonym.get("race4").getValue();
-//			
-//		}
+		Boolean isInterceptUsed = true;
+		for (String statsVariable : variables) {
+			
+			// intercept
+			if (isInterceptUsed == true) {
+				isInterceptUsed = false;
+				value = ((StatsOutput) hashtable.get("Intercept")).getEstimate();
+				logitP += value;
+				System.out.println("Intercept value: " + value);
+				System.out.println("current logitP value = " + logitP);
+				System.out.println(" ~~~~~ ");
 
-		System.out.println("@@@@@ END GENERIC LOOKUPS @@@@@");
-		System.out.println("\n ---------- \n");
+			}
+			
+//			int race = ta.getRace();
+//			String racestring = "race4"+Integer.toString(race);
+//			if (hashtable.containsKey(racestring)) {
+//				value = ((StatsOutput) hashtable.get(racestring)).getEstimate();
+//				logitP += value;
+//				System.out.println("value: " + value);
+//				System.out.println(logitP);
+//			}
+
+			// interaction
+			// interaction is checked first otherwise the following line used
+			// for categorical variable will throw an error
+			// String agentVariable = variableSynonym.get(statsVariable).toString().trim();
+			System.out.println(statsVariable);
+			if (statsVariable.contains("*")){
+				System.out.println("~~~~~ in the interaction variable if statment ~~~~~");
+				String[] interaction = statsVariable.split("\\*");
+
+				System.out.println(statsVariable + " is an interaction between "
+						+ interaction[0] + " and " + interaction[1]);
+				// example key: agecat*race422
+				
+				// convert variable in stats output into variable used for agent
+				String agentVariable1 = variableSynonym.get(interaction[0]).toString().trim();
+				String agentVariable2 = variableSynonym.get(interaction[1]).toString().trim();
+				System.out.println("interaction variable conversion: " + agentVariable1 + " " + agentVariable2);
+				
+			} else {
+				
+				// categorical variable
+				// convert variable in stats output into variable used for agent
+				String agentVariable = variableSynonym.get(statsVariable).toString().trim();
+				int categoricalInt = ta.getValue(agentVariable);
+				String categoricalString = statsVariable+Integer.toString(categoricalInt);
+				
+
+				System.out.println("catint:" + categoricalInt + " " + "catstring:"
+						+ categoricalString + " " + "agentVariable:"
+						+ agentVariable + " " + "statsVariable:" + statsVariable);
+				
+				
+				if (hashtable.containsKey(categoricalString)) {
+					System.out.println("~~~~~ in the cat variable if statement ~~~~~");
+					value = ((StatsOutput) hashtable.get(categoricalString)).getEstimate();
+					System.out.println("hashtable key: " + value);
+					
+					logitP += value;
+					System.out.println("value: " + value);
+					System.out.println("current logitP value = " + logitP);
+				}
+				
+//				if (variableSynonym.containsKey(variable)) {
+//					System.out.println("input: stats variable = " + variable
+//							+ "; output: maps to agent variable = " + agentVariable);
+//					
+//					value = ((StatsOutput) hashtable.get("agecat")).getEstimate();
+//					System.out.println(value);
+//					
+//					logitP += value;
+//					System.out.println("value: " + value);
+//					System.out.println(logitP);
+//				}
+				
+				
+//				System.out.println("printing agent variable: " + agentVariable);
+//				if (statsVariable.contains("*")) {
+//					System.out.println("~~~~~ in the interaction variable if statment ~~~~~");
+//					String[] interaction = statsVariable.split("\\*");
+	//
+//					System.out.println(statsVariable + " is an interaction between "
+//							+ interaction[0] + " and " + interaction[1]);
+//				}
+			}
+
+		}
 	}
 	
+	/**
+	 * 
+	 * @param ta
+	 * @param variables
+	 * @param alternativeName
+	 * @param file
+	 * @param rowStart
+	 * @param rowEnd
+	 * @param colStart
+	 * @param colEnd
+	 * @throws IOException
+	 * @deprecated Replaced by {@link #genericCodedLookups()
+	 */
+	@Deprecated
 	public static void hardCodedLookups (TestAgent ta,
 			String[] variables, String alternativeName, String file,
 			int rowStart, int rowEnd, int colStart, int colEnd)
@@ -134,7 +280,7 @@ public class ProbabilityFromRegressionOutput {
 		int i = 0;
 		// int start = 5;
 		// int end = 15;
-		// assumes first 3 rows are the variable, categorical status 1, cat status 2
+		// assumes first 3 rows are: the variable, categorical status 1, cat status 2
 		// last row is the column of beta estimates
 		// puts data into hashtable
 		while ((nextLine = reader.readNext()) != null) {
@@ -170,6 +316,7 @@ public class ProbabilityFromRegressionOutput {
 		
 		if (hashtable.containsKey("Intercept")) {
 			value = ((StatsOutput) hashtable.get("Intercept")).getEstimate();
+			System.out.println(value);
 			logitP += value;
 			System.out.println("value: " + value);
 			System.out.println(logitP);
@@ -178,8 +325,10 @@ public class ProbabilityFromRegressionOutput {
 		
 		int age = ta.getAgeCat();
 		String agestring = "agecat"+Integer.toString(age);
+		System.out.println(agestring);
 		if (hashtable.containsKey(agestring)) {
 			value = ((StatsOutput) hashtable.get(agestring)).getEstimate();
+			System.out.println(value);
 			logitP += value;
 			System.out.println("value: " + value);
 			System.out.println(logitP);
